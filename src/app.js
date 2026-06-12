a949dc7c0ccd33483ad9d36e9bdd09098badc581
yupei.wu@rd.shareuav.cn <495174699@qq.com>
2026-06-12 13:47:06 +0800

    feat: mount spin lottery router in app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { notFoundHandler, errorHandler } from './middlewares/errorHandler.js';
 import healthRouter from './routes/health.js';
 import authRouter from './routes/auth.js';
 import lotteryRouter from './routes/lottery.js';
+import spinRouter from './routes/spin.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(requestLogger);
 app.use(healthRouter);
 app.use(authRouter);
 app.use(lotteryRouter);
+app.use(spinRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
